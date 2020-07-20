0ca799120fd4b53207596a6c0d0f4bc37b9dcb8f
Hristo Roque <aroquega@unsa.edu.pe>
2020-07-20 15:03:14 -0500

    Añadiendo las clases y layouts de ListView y DetailView de vídeos.

--- app/src/main/java/com/example/fisiovideos/VideoDetailView.java
+++ app/src/main/java/com/example/fisiovideos/VideoDetailView.java
@@ -6,12 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class VideoDetailView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_video_detail_view);
     }
 
     public void goToVideoListView(View view){
--- app/src/main/java/com/example/fisiovideos/VideoListView.java
+++ app/src/main/java/com/example/fisiovideos/VideoListView.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class VideoListView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_video_list_view);
     }
 
-    public void goToVideoListView(View view){
-        Intent intent = new Intent(this,VideoListView.class);
+    public void goToVideoDetailView(View view){
+        Intent intent = new Intent(this,VideoDetailView.class);
         startActivity(intent);
     }
 }
