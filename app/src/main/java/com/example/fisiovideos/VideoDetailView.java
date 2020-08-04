a877cfffb1060af2c9d123fba22cde3b2f9c00bf
Iam Contreras Alcazar <iamcontrerasalcazar@gmail.com>
2020-08-04 01:13:55 -0500

    change version of realtimedatabase

--- app/src/main/java/com/example/fisiovideos/VideoDetailView.java
+++ app/src/main/java/com/example/fisiovideos/VideoDetailView.java
@@ -1,23 +1,88 @@
 package com.example.fisiovideos;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.fisiovideos.video.Video;
 import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class VideoDetailView extends AppCompatActivity {
+    private static final String PATH_VIDEOS = "videos";
+    String user_name;
+
+    private TextView tv_tittle;
+    private TextView tv_description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_detail_view);
+        user_name = getIntent().getStringExtra("USER");
+
+        initialComponents();
+        getFirebaseData();
+    }
+
+    private void initialComponents() {
+        tv_tittle = (TextView)findViewById(R.id.title);
+        tv_description = (TextView)findViewById(R.id.description);
+    }
+
+    private void getFirebaseData() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference = database.getReference(PATH_VIDEOS);
+
+        reference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String previousChildName) {
+
+                Video video_data = dataSnapshot.getValue(Video.class);
+                video_data.setId(dataSnapshot.getKey());
+                if(video_data.getName().equals(user_name)){
+                    //Log.d("tag", "IGUALEEEEEEEEEEEEEEEEEEEEEEEEEEEES " + user_name);
+                    //Log.d("tag", "IGUALEEEEEEEEEEEEEEEEEEEEEEEEEEEES " + video_data.getDescription() +" " + video_data.getName());
+                    tv_tittle.setText(video_data.getName());
+                    tv_description.setText(video_data.getDescription());
+                }
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     public void goToVideoListView(View view){
--- app/src/main/java/com/example/fisiovideos/VideoListView.java
+++ app/src/main/java/com/example/fisiovideos/VideoListView.java
@@ -1,23 +1,40 @@
 package com.example.fisiovideos;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.example.fisiovideos.video.Video;
 import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class VideoListView extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<String> nombre_ejercicios;
+    private static final String PATH_VIDEOS = "videos";
+    private static final String PATH_VIDEO = "video";
+    private static final String PATH_NAME = "name";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +45,31 @@ public class VideoListView extends AppCompatActivity {
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-
-        VideoItemAdapter adapter = new VideoItemAdapter(new String[]{"Hristo Roque", "Iam Contreras", "Solansh"});
+        nombre_ejercicios = new ArrayList<String>();
+        agregarEjercicios(nombre_ejercicios);
+        VideoItemAdapter adapter = new VideoItemAdapter(nombre_ejercicios);
         recyclerView.setAdapter(adapter);
+    }
+
+    private void agregarEjercicios(final ArrayList<String> nombre_ejercicios) {
+
+        Video video1 = new Video("video1","algo de este video supongo");
+        Video video2 = new Video("video2","algo de este video supongo 2");
+        Video video3 = new Video("video3","algo de este video supongo 2");
+
+        /*
+       FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference  reference = database.getReference(PATH_VIDEOS);
+
+
+        reference.push().setValue(video1);
+        reference.push().setValue(video2);
+        reference.push().setValue(video3);
+        */
+
+        nombre_ejercicios.add(video1.getName());
+        nombre_ejercicios.add(video2.getName());
+        nombre_ejercicios.add(video3.getName());
     }
 
     public void goToVideoDetailView(View view){
