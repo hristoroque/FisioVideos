d865db1ceda5aa7ded4bbf97c51ae24fe4fec73e
Hristo Roque <aroquega@unsa.edu.pe>
2020-08-10 07:49:30 -0500

    Añadiendo las vistas de crear vídeos y las de crear pacientes para el artículo

---
+++ app/src/main/java/com/example/fisiovideos/VideoCreateView.java
@@ -0,0 +1,14 @@
+package com.example.fisiovideos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VideoCreateView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_create_view);
+    }
+}
